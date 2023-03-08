bb561c4b2604f40956d5c2307060cf2da66937d9
JamshidXolmominov <xolmominovj2023@gmail.com>
2023-03-09 00:23:25 +0300

    Protect page

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -4,10 +4,11 @@ import { useContext, useEffect, useState } from 'react';
 import { AiOutlineSearch, AiOutlineUser, AiOutlineLogout } from 'react-icons/ai';
 import { BiBellMinus } from 'react-icons/bi';
 import { AuthContext } from 'src/context/auth.context';
+import { useAuth } from 'src/hooks/useAuth';
 
 const Header = () => {
 	const [scrolled, setScrolled] = useState(false);
-	const { logout } = useContext(AuthContext);
+	const { logout } = useAuth();
 
 	useEffect(() => {
 		const handleScroll = () => {
@@ -17,13 +18,17 @@ const Header = () => {
 				setScrolled(false);
 			}
 		};
+
 		window.addEventListener('scroll', handleScroll);
+
 		return () => window.removeEventListener('scroll', handleScroll);
 	}, []);
+
 	return (
 		<header className={`${scrolled && 'bg-[#141414] shadow-lg'}`}>
 			<div className='flex items-center space-x-2 md:space-x-10'>
 				<Image src={'/logo.svg'} alt={'logo'} width={56} height={56} className={'cursor-pointer object-contain'} />
+
 				<ul className='space-x-4 md:flex hidden'>
 					<li className='navLink'>Home</li>
 					<li className='navLink'>Movies</li>
@@ -32,6 +37,7 @@ const Header = () => {
 					<li className='navLink'>Popular</li>
 				</ul>
 			</div>
+
 			<div className='flex items-center space-x-4 text-sm font-light'>
 				<AiOutlineSearch className='h-6 w-6 cursor-pointer' />
 				<p className='hidden lg:inline'>Kids</p>
@@ -44,4 +50,5 @@ const Header = () => {
 		</header>
 	);
 };
+
 export default Header;
--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -3,13 +3,17 @@ import Cookies from 'js-cookie';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
 import { auth } from 'src/firebase';
+
 export const useAuth = () => {
 	const [isLoading, setIsLoading] = useState<boolean>(false);
 	const [error, setError] = useState<string>('');
 	const [user, setUser] = useState<User | null>(null);
+
 	const router = useRouter();
+
 	const signUp = async (email: string, password: string) => {
 		setIsLoading(true);
+
 		await createUserWithEmailAndPassword(auth, email, password)
 			.then(res => {
 				setUser(res.user);
@@ -25,8 +29,10 @@ export const useAuth = () => {
 			.catch(error => setError(error.message))
 			.finally(() => setIsLoading(false));
 	};
+
 	const signIn = async (email: string, password: string) => {
 		setIsLoading(true);
+
 		await signInWithEmailAndPassword(auth, email, password)
 			.then(res => {
 				setUser(res.user);
@@ -37,16 +43,19 @@ export const useAuth = () => {
 			.catch(error => setError(error.message))
 			.finally(() => setIsLoading(false));
 	};
+
 	const logout = async () => {
 		setIsLoading(true);
 
 		signOut(auth)
 			.then(() => {
-				setUser(null);
 				Cookies.remove('user_id');
+				setUser(null);
+				router.push('/auth');
 			})
 			.catch(error => setError(error.message))
 			.finally(() => setIsLoading(false));
 	};
+
 	return { error, isLoading, user, signIn, signUp, logout, setUser, setIsLoading };
 };
