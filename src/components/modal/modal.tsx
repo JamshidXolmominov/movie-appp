05e2212f5e8fb4052a9d4ff82f05670eab9dbd77
JamshidXolmominov <xolmominovj2023@gmail.com>
2023-03-09 17:17:53 +0300

    Overview

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -7,6 +7,7 @@ import ReactPlayer from 'react-player';
 import { BiPlus } from 'react-icons/bi';
 import { BsVolumeMute, BsVolumeDown } from 'react-icons/bs';
 import { AiOutlineLike } from 'react-icons/ai';
+
 const Modal = () => {
 	const { modal, setModal, currentMovie } = useInfoStore();
 	const [trailer, setTrailer] = useState<string>('');
@@ -15,23 +16,30 @@ const Modal = () => {
 
 	const base_url = process.env.NEXT_PUBLIC_API_DOMAIN as string;
 	const api_key = process.env.NEXT_PUBLIC_API_KEY as string;
+
 	const api = `${base_url}/${currentMovie?.media_type === 'tv' ? 'tv' : 'movie'}/${
 		currentMovie.id
 	}/videos?api_key=${api_key}&language=en-US`;
+
 	const handleClose = () => {
 		setModal(false);
 	};
+
 	useEffect(() => {
 		const fetchVideoData = async () => {
 			const data = await fetch(api).then(res => res.json());
+
 			if (data?.results) {
 				const index = data.results.findIndex((el: Element) => el.type === 'Trailer');
 				setTrailer(data?.results[index]?.key);
 			}
 		};
+
 		fetchVideoData();
+
 		// eslint-disable-next-line
 	}, [currentMovie]);
+
 	return (
 		<MuiModal
 			open={modal}
@@ -45,6 +53,7 @@ const Modal = () => {
 				>
 					<FaTimes />
 				</button>
+
 				<div className='relative pt-[55%]'>
 					<ReactPlayer
 						url={`https://www.youtube.com/watch?v=${trailer}`}
@@ -54,7 +63,7 @@ const Modal = () => {
 						style={{ position: 'absolute', top: 0, left: 0 }}
 						muted={muted}
 					/>
-					<div className='absolute bottom-10 left-10 flex w-full items-center justify-between px-18'>
+					<div className='absolute bottom-10 left-10 right-10 flex w-full items-center justify-between px-18'>
 						<div className='flex space-x-2'>
 							<button
 								onClick={() => setPlaying(prev => !prev)}
@@ -78,12 +87,13 @@ const Modal = () => {
 							<button className='modalButton'>
 								<AiOutlineLike className='w-7 h-7' />
 							</button>
-							<button className='modalButton' onClick={() => setMuted(prev => !prev)}>
-								{muted ? <BsVolumeMute className='w-7 h-7' /> : <BsVolumeDown className='w-7 h-7' />}
-							</button>
 						</div>
+						<button className='modalButton absolute right-12' onClick={() => setMuted(prev => !prev)}>
+							{muted ? <BsVolumeMute className='w-7 h-7' /> : <BsVolumeDown className='w-7 h-7' />}
+						</button>
 					</div>
 				</div>
+
 				<div className='flex space-x-16 rounded-b-md bg-[#181818] px-10 py-8'>
 					<div className='space-y-6 text-lg'>
 						<div className='flex items-center space-x-2 text-sm'>
@@ -91,12 +101,14 @@ const Modal = () => {
 							<p className='font-light'>{currentMovie?.release_date}</p>
 							<div className='flex h-4 items-center justify-center rounded border border-white/40 px-1.5 text-xs'>HD</div>
 						</div>
+
 						<div className='flex flex-col gap-x-10 gap-y-4 font-light md:flex-row'>
 							<p className='w-5/6'>{currentMovie?.overview}</p>
 							<div className='flex flex-col space-y-3 text-sm'>
 								<div>
 									<span className='text-[gray]'>Original language:</span> {currentMovie?.original_language}
 								</div>
+
 								<div>
 									<span className='text-[gray]'>Total votes:</span> {currentMovie?.vote_count}
 								</div>
@@ -108,4 +120,5 @@ const Modal = () => {
 		</MuiModal>
 	);
 };
+
 export default Modal;
