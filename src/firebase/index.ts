d54ea1bc1ece5f5cb8ea1ac5201794287d547828
JamshidXolmominov <xolmominovj2023@gmail.com>
2023-06-06 22:30:12 +0300

    update firebaseConfig

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBgQlJGVqfAfU_cZMK7ZqTNkymn5r9wjoQ",
-  authDomain: "movie-apps-be03c.firebaseapp.com",
-  projectId: "movie-apps-be03c",
-  storageBucket: "movie-apps-be03c.appspot.com",
-  messagingSenderId: "469126757728",
-  appId: "1:469126757728:web:f642c13638b48b38c97205",
-  measurementId: "G-87NW61Y8C7"
+  apiKey: "AIzaSyCjyEPjDi9VP3Lp3mVsDAr3qP7GjOFcoHc",
+  authDomain: "movie-a5ee2.firebaseapp.com",
+  projectId: "movie-a5ee2",
+  storageBucket: "movie-a5ee2.appspot.com",
+  messagingSenderId: "1078995558267",
+  appId: "1:1078995558267:web:8304ef116fe92d37d867de",
+  measurementId: "G-YX51Y5XG5Y"
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
